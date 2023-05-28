ae91d97a796caddb367e31872b761b3e28a8ab78
Sammieblz <samuelndubuisi32@gmail.com>
2023-05-28 17:51:36 -0400

    456

--- chatgpt-clone/src/App.js
+++ chatgpt-clone/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TESTING TESTING 123
+          TESTING TESTING 456
         </p>
         <a
           className="App-link"
